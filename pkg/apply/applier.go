b7d56406b670821bf5425b24290ce8f247bd1c83
Morten Torkildsen <mortent@google.com>
2020-03-12 11:31:24 -0700

    [WIP] Implement applier as a task queue

--- pkg/apply/applier.go
+++ pkg/apply/applier.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"sort"
+	"time"
 
 	"github.com/go-errors/errors"
 	"github.com/spf13/cobra"
@@ -158,99 +159,301 @@ func (a *Applier) Run(ctx context.Context) <-chan event.Event {
 		// The adapter is used to intercept what is meant to be printing
 		// in the ApplyOptions, and instead turn those into events.
 		a.ApplyOptions.ToPrinter = adapter.toPrinterFunc()
-		// This provides us with a slice of all the objects that will be
-		// applied to the cluster.
-		infos, err := a.ApplyOptions.GetObjects()
+
+		// Set the info objects on the ApplyOptions.
+		infos, err := a.setInfos()
 		if err != nil {
 			ch <- event.Event{
 				Type: event.ErrorType,
 				ErrorEvent: event.ErrorEvent{
-					Err: errors.WrapPrefix(err, "error reading resource manifests", 1),
+					Err: err,
 				},
 			}
 			return
 		}
 
-		// Validate the objects are in the same namespace.
-		if !validateNamespace(infos) {
-			err := fmt.Errorf("currently, applied objects must be in the same namespace")
-			ch <- event.Event{
-				Type: event.ErrorType,
-				ErrorEvent: event.ErrorEvent{
-					Err: errors.WrapPrefix(err, "objects have differing namespaces", 1),
-				},
-			}
-			return
-		}
+		identifiers := infosToObjMetas(infos)
 
-		// sort the info objects starting from independent to dependent objects,
-		// and set them back ordering precedence can be found in resource_infos.go
-		sort.Sort(ResourceInfos(infos))
-		a.ApplyOptions.SetObjects(infos)
+		//statusCtx, cancelFunc := context.WithCancel(context.Background())
+		statusChannel := a.statusPoller.Poll(ctx, identifiers, polling.Options{
+			PollUntilCancelled: true,
+			PollInterval:       a.StatusOptions.period,
+			UseCache:           true,
+			DesiredStatus:      status.CurrentStatus,
+		})
 
-		err = a.ApplyOptions.Run()
-		if err != nil {
-			// If we see an error here we just report it on the channel and then
-			// give up. Eventually we might be able to determine which errors
-			// are fatal and which might allow us to continue.
-			ch <- event.Event{
-				Type: event.ErrorType,
-				ErrorEvent: event.ErrorEvent{
-					Err: errors.WrapPrefix(err, "error applying resources", 1),
-				},
-			}
-			return
-		}
-		// If we get there, then all resources have been successfully applied.
-		ch <- event.Event{
-			Type: event.ApplyType,
-			ApplyEvent: event.ApplyEvent{
-				Type: event.ApplyEventCompleted,
+		tasks := []task{
+			waitTask{
+				identifiers: identifiers,
+				condition: allKnown,
+				timeout: 30 * time.Second,
+			},
+			applyTask{
+				objects: infos,
+			},
+			waitTask{
+				identifiers: identifiers,
+				condition: allCurrent,
+				timeout: 4 * time.Minute,
+			},
+			pruneTask{
+				objects: infos,
 			},
 		}
+		taskQueue := make(chan task, len(tasks))
+		for _, t := range tasks {
+			taskQueue <- t
+		}
 
-		if a.StatusOptions.wait {
-			statusChannel := a.statusPoller.Poll(ctx, infosToObjMetas(infos), polling.Options{
-				PollUntilCancelled: false,
-				PollInterval:       a.StatusOptions.period,
-				UseCache:           true,
-				DesiredStatus:      status.CurrentStatus,
-			})
-			// As long as the statusChannel remains open, we take every statusEvent,
-			// wrap it in an Event and send it on the channel.
-			// TODO: What should we do if waiting for status times out? We currently proceed with
-			// prune, but that doesn't seem right.
-			for statusEvent := range statusChannel {
+		aggregator := newAggregator(identifiers)
+
+		taskChannel := make(chan taskResult)
+		timeoutChannel := make(chan struct{})
+
+		currentTask, done := a.nextTask(taskQueue, aggregator)
+		if done {
+			return
+		}
+		a.startTask(currentTask, taskChannel, timeoutChannel, ch)
+
+		for {
+			select {
+			case statusEvent := <- statusChannel:
+				fmt.Print("Message on status channel\n")
 				ch <- event.Event{
 					Type:        event.StatusType,
 					StatusEvent: statusEvent,
 				}
-			}
-		}
-
-		if !a.NoPrune {
-			err = a.PruneOptions.Prune(infos, ch)
-			if err != nil {
-				// If we see an error here we just report it on the channel and then
-				// give up. Eventually we might be able to determine which errors
-				// are fatal and which might allow us to continue.
+				aggregator.resourceStatus(statusEvent.Resource.Identifier, statusEvent.Resource.Status)
+				if wt, ok := currentTask.task.(waitTask); ok {
+					fmt.Print("We have a wait task\n")
+					if aggregator.conditionMet(wt.identifiers, wt.condition) {
+						fmt.Print("Condition met. Cancelling timeout\n")
+						currentTask.timeoutHandle()
+						currentTask, done = a.nextTask(taskQueue, aggregator)
+						if done {
+							return
+						}
+						a.startTask(currentTask, taskChannel, timeoutChannel, ch)
+					}
+				}
+			case msg := <- taskChannel:
+				fmt.Print("Message on task channel\n")
+				if msg.err != nil {
+					ch <- event.Event{
+						Type: event.ErrorType,
+						ErrorEvent: event.ErrorEvent{
+							Err: msg.err,
+						},
+					}
+					return
+				}
+				currentTask, done = a.nextTask(taskQueue, aggregator)
+				if done {
+					return
+				}
+				a.startTask(currentTask, taskChannel, timeoutChannel, ch)
+			case <- timeoutChannel:
+				fmt.Print("Message on timeout channel\n")
 				ch <- event.Event{
 					Type: event.ErrorType,
 					ErrorEvent: event.ErrorEvent{
-						Err: errors.WrapPrefix(err, "error pruning resources", 1),
+						Err: fmt.Errorf("timeout reached for task"),
 					},
 				}
 				return
 			}
-			ch <- event.Event{
-				Type: event.PruneType,
-				PruneEvent: event.PruneEvent{
-					Type: event.PruneEventCompleted,
-				},
-			}
 		}
 	}()
 	return ch
+}
+
+func (a *Applier) nextTask(taskQueue chan task, aggregator *aggregator) (*runningTask, bool) {
+	fmt.Print("Creating next task\n")
+	for {
+		select {
+		case t := <-taskQueue:
+			if wt, ok := t.(waitTask); ok {
+				fmt.Print("Next task is a wait task\n")
+				if aggregator.conditionMet(wt.identifiers, wt.condition) {
+					fmt.Print("All conditions are already met\n")
+					continue
+				}
+			}
+			return &runningTask{
+				task: t,
+			}, false
+		default:
+			fmt.Print("No tasks left\n")
+			return nil, true
+		}
+	}
+}
+
+func (a *Applier) startTask(rt *runningTask, taskChannel chan taskResult, timeoutChannel chan struct{}, eventChannel chan event.Event) {
+	switch t := rt.task.(type) {
+	case applyTask:
+		fmt.Print("Starting apply task\n")
+		a.ApplyOptions.SetObjects(t.objects)
+		go func() {
+			err := a.ApplyOptions.Run()
+			taskChannel <- taskResult{
+				err: err,
+			}
+		}()
+	case pruneTask:
+		fmt.Print("Starting prune task\n")
+		go func() {
+			err := a.PruneOptions.Prune(t.objects, eventChannel)
+			taskChannel <- taskResult{
+				err: err,
+			}
+		}()
+	case waitTask:
+		fmt.Print("Starting wait task\n")
+		rt.timeoutHandle = setTimeout(t.timeout, timeoutChannel)
+	}
+}
+
+type runningTask struct {
+	task task
+	timeoutHandle func()
+}
+
+type task interface{}
+
+type applyTask struct{
+	objects []*resource.Info
+}
+
+type pruneTask struct{
+	objects []*resource.Info
+}
+
+type waitTask struct{
+	identifiers []object.ObjMetadata
+	condition   condition
+	timeout time.Duration
+}
+
+type taskResult struct {
+	err error
+}
+
+type condition string
+
+const (
+	allKnown    condition = "AllKnown"
+	allCurrent  condition = "AllCurrent"
+	allNotFound condition = "AllNotFound"
+)
+
+func setTimeout(timeout time.Duration, channel chan struct{}) func() {
+	timer := time.NewTimer(timeout)
+	go func() {
+		<-timer.C
+		fmt.Print("Timeout triggered\n")
+		channel <- struct{}{}
+	}()
+	return func() {
+		timer.Stop()
+	}
+}
+
+func newAggregator(identifiers []object.ObjMetadata) *aggregator {
+	rm := make(map[object.ObjMetadata]resourceInfo)
+
+	for _, obj := range identifiers {
+		rm[obj] = resourceInfo{
+			Identifier: obj,
+			CurrentStatus: status.UnknownStatus,
+		}
+	}
+	return &aggregator{
+		resourceMap: rm,
+	}
+}
+
+type aggregator struct {
+	resourceMap map[object.ObjMetadata]resourceInfo
+}
+
+type resourceInfo struct {
+	Identifier object.ObjMetadata
+	CurrentStatus status.Status
+}
+
+func (a *aggregator) resourceStatus(identifier object.ObjMetadata, s status.Status) {
+	if ri, found := a.resourceMap[identifier]; found {
+		ri.CurrentStatus = s
+		a.resourceMap[identifier] = ri
+	}
+}
+
+func (a *aggregator) conditionMet(identifiers []object.ObjMetadata, c condition) bool {
+	switch c {
+	case allCurrent:
+		return a.allMatchStatus(identifiers, status.CurrentStatus)
+	case allNotFound:
+		return a.allMatchStatus(identifiers, status.NotFoundStatus)
+	default:
+		return a.noneMatchStatus(identifiers, status.UnknownStatus)
+	}
+}
+
+func (a *aggregator) allMatchStatus(identifiers []object.ObjMetadata, s status.Status) bool {
+	fmt.Print("Checking all match status\n")
+	for id, ri := range a.resourceMap {
+		if contains(identifiers, id) {
+			if ri.CurrentStatus != s {
+				fmt.Printf("Resource %s has status %s\n", id.GroupKind.String(), ri.CurrentStatus.String())
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (a *aggregator) noneMatchStatus(identifiers []object.ObjMetadata, s status.Status) bool {
+	fmt.Print("Checking non match status\n")
+	for id, ri := range a.resourceMap {
+		if contains(identifiers, id) {
+			if ri.CurrentStatus == s {
+				fmt.Printf("Resource %s has status %s\n", id.GroupKind.String(), ri.CurrentStatus.String())
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func contains(identifiers []object.ObjMetadata, id object.ObjMetadata) bool {
+	for _, identifier := range identifiers {
+		if identifier.Equals(&id) {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *Applier) setInfos() ([]*resource.Info, error) {
+	// This provides us with a slice of all the objects that will be
+	// applied to the cluster.
+	infos, err := a.ApplyOptions.GetObjects()
+	if err != nil {
+		return nil, errors.WrapPrefix(err, "error reading resource manifests", 1)
+	}
+
+	// Validate the objects are in the same namespace.
+	if !validateNamespace(infos) {
+		return nil, fmt.Errorf("currently, applied objects must be in the same namespace")
+	}
+
+	// sort the info objects starting from independent to dependent objects,
+	// and set them back ordering precedence can be found in resource_infos.go
+	sort.Sort(ResourceInfos(infos))
+	a.ApplyOptions.SetObjects(infos)
+	return infos, nil
 }
 
 func infosToObjMetas(infos []*resource.Info) []object.ObjMetadata {
